aec4b140d440cbf46734fa0eb4168bcb7e2f8f37
Maciej Czernek <maciejczernek@gmail.com>
2024-02-15 12:51:01 +0100

    Modify collection

--- request_lib/req_collection.py
+++ request_lib/req_collection.py
@@ -2,33 +2,40 @@ import os
 import requests
 import allure
 
+from tests.config import AllureConfig
+
 
 class JsonPlaceholder:
     BASE_URL = "https://jsonplaceholder.typicode.com/"
 
     def __init__(self):
         self.timeout = 5
+        self.allure_utils = AllureConfig()
 
     @allure.step("GET resource")
     def get_resource(self, id):
         url = os.path.join(self.BASE_URL, f"posts/{id}")
-        allure.attach(url, "url")
-        return requests.get(url, timeout=self.timeout)
+        r = requests.get(url, timeout=self.timeout)
+        self.allure_utils.attach_info(r)
+        return r
 
     @allure.step("GET all resources")
     def get_all_resource(self):
         url = os.path.join(self.BASE_URL, f"posts")
-        allure.attach(url, "url")
-        return requests.get(url, timeout=self.timeout)
+        r = requests.get(url, timeout=self.timeout)
+        self.allure_utils.attach_info(r)
+        return r
 
     @allure.step("DELETE resource")
     def delete_resource(self, id):
         url = os.path.join(self.BASE_URL, f"posts/{id}")
-        allure.attach(url, "url")
-        return requests.delete(url, timeout=self.timeout)
+        r = requests.delete(url, timeout=self.timeout)
+        self.allure_utils.attach_info(r)
+        return r
 
     @allure.step("POST resource")
     def post_resource(self, data):
         url = os.path.join(self.BASE_URL, f"posts")
-        allure.attach(url, "url")
-        return requests.post(url, data=data, timeout=self.timeout)
+        r = requests.post(url, data=data, timeout=self.timeout)
+        self.allure_utils.attach_info(r)
+        return r
