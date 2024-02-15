6e85f1edf869d76feb6376f4a4b722922e086145
Maciej Czernek <maciejczernek@gmail.com>
2024-02-15 09:38:12 +0100

    Refactor conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,31 +3,34 @@ import sys
 import http
 import allure
 
-from utilities.file_op import create_report_folder, copy_allure_history, \
-    generate_report, create_response_log_folder
 from http.client import HTTPConnection
-
-RESULTS_PATH = "../test_results/allure-results/logs"
+from .config import AllureConfig
 
 
 @pytest.fixture(scope="function", autouse=True)
 def activate_logging(request):
+    allure_config = AllureConfig()
     test_name = request.node.name
     http.client.HTTPConnection.debuglevel = 1
     stdout_origin = sys.stdout
-    sys.stdout = open(f"{RESULTS_PATH}/{test_name}.txt", "w")
+    sys.stdout = open(f"{allure_config.RESPONSE_LOGS}/{test_name}.txt", "w")
     yield
     sys.stdout.close()
     sys.stdout = stdout_origin
-    allure.attach.file(f"{RESULTS_PATH}/{test_name}.txt", name="LOG")
+    allure.attach.file(
+        source=f"{allure_config.RESPONSE_LOGS}/{test_name}.txt",
+        name="LOG"
+    )
 
 
 @pytest.fixture(scope="session", autouse=True)
 def create_log_folder():
-    create_response_log_folder()
+    allure_config = AllureConfig()
+    allure_config.create_response_log_folder()
 
 
 def pytest_sessionfinish():
-    create_report_folder()
-    copy_allure_history()
-    generate_report()
+    allure_config = AllureConfig()
+    allure_config.create_report_folder()
+    allure_config.copy_allure_history()
+    allure_config.generate_report()
